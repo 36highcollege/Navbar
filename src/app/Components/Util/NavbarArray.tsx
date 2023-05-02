4ddbdc65edcf6e0acff1f104e1adee2902e847b8
Abdul-Basit <112763949+basitgggg@users.noreply.github.com>
2023-05-02 23:31:24 +0500

    fixed some issues

--- src/app/Components/Util/NavbarArray.tsx
+++ src/app/Components/Util/NavbarArray.tsx
@@ -32,8 +32,8 @@ export let Navbaritem: Array<Barvalue> = [
 export default function Navitem() {
     return (
         <div className="hidden sm:flex items-center justify-center space-x-6">
-            {Navbaritem.map((item: any) => (
-                <div className="hover:underline hover:text-gray-800 font-semibold text-lg group">
+            {Navbaritem.map((item: any , index:any) => (
+                <div key={index+44} className="hover:underline hover:text-gray-800 font-semibold text-lg group">
                     <div className="cursor-pointer flex items-center select-none">
                         {item.label} {item.dropdown ? <div className=" group-hover:rotate-180 duration-500 mt-1"> <BiCaretDown /></div> : ""}
                     </div>
